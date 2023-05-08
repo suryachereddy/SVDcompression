901f38ab14e5b7adbab1a8e5b59a26ca8852ea6c
suryachereddy <suryachereddy@gmail.com>
2023-05-08 15:50:19 -0700

    hello world

---
+++ include/hello.h
@@ -0,0 +1,7 @@
+#include <string>
+
+std::string get_message()
+{
+    std::string message = "Hello, World!";
+    return message;
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <hello.h>
+
+int main()
+{
+    std::cout << get_message() << std::endl;
+}
