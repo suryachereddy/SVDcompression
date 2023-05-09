a569e9b6557dfe7789d56bb8da4deb770b684b70
suryachereddy <suryachereddy@gmail.com>
2023-05-08 18:57:31 -0700

    load image

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-#include <hello.h>
-
-int main()
+#include <opencv2/opencv.hpp>
+int main(int argc, char **argv)
 {
-    std::cout << get_message() << std::endl;
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <image path>" << std::endl;
+        return -1;
+    }
+    cv::Mat image;
+    std::string image_path = argv[1];
+    image = cv::imread(image_path);
+
+    // display image
+    cv::imshow("Image", image);
+    cv::waitKey(0);
+    return 0;
 }
