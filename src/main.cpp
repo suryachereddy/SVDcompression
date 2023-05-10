129b1f86e1f9909fad266121790925116c5704d2
suryachereddy <suryachereddy@gmail.com>
2023-05-10 02:11:25 -0700

    compress with 10 singular values

--- src/main.cpp
+++ src/main.cpp
@@ -32,14 +32,50 @@ int main(int argc, char **argv)
     cv::cv2eigen(cr, er);
     std::cout << std::endl
               << "Calculating SVD for blue channel" << std::endl;
-    Eigen::BDCSVD<Eigen::MatrixXd> svdb(eb, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::BDCSVD<Eigen::MatrixXd> svdb(eb, Eigen::ComputeFullU | Eigen::ComputeFullV);
 
     std::cout << std::endl
               << "Calculating SVD for green channel" << std::endl;
-    Eigen::BDCSVD<Eigen::MatrixXd> svdg(eg, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::BDCSVD<Eigen::MatrixXd> svdg(eg, Eigen::ComputeFullU | Eigen::ComputeFullV);
 
     std::cout << std::endl
               << "Calculating SVD for red channel" << std::endl;
-    Eigen::BDCSVD<Eigen::MatrixXd> svdr(er, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::BDCSVD<Eigen::MatrixXd> svdr(er, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+    // reconstructing image using first 10 singular values
+    std::cout << std::endl
+              << "Reconstructing image" << std::endl;
+    // blue channel
+    Eigen::MatrixXd reconb = svdb.matrixU().leftCols(10) * svdb.singularValues().head(10).asDiagonal() * svdb.matrixV().leftCols(10).transpose();
+    // green channel
+    Eigen::MatrixXd recong = svdg.matrixU().leftCols(10) * svdg.singularValues().head(10).asDiagonal() * svdg.matrixV().leftCols(10).transpose();
+    // red channel
+    Eigen::MatrixXd reconr = svdr.matrixU().leftCols(10) * svdr.singularValues().head(10).asDiagonal() * svdr.matrixV().leftCols(10).transpose();
+
+    // eigen to cv
+    cv::Mat reconb_cv, recong_cv, reconr_cv;
+    cv::eigen2cv(reconb, reconb_cv);
+    cv::eigen2cv(recong, recong_cv);
+    cv::eigen2cv(reconr, reconr_cv);
+
+    // merge channels
+    std::vector<cv::Mat> channels;
+    channels.push_back(reconb_cv);
+    channels.push_back(recong_cv);
+    channels.push_back(reconr_cv);
+    cv::Mat recon;
+    cv::merge(channels, recon);
+    // normalize pixel values to [0, 255]
+    cv::normalize(recon, recon, 0, 255, cv::NORM_MINMAX);
+
+    // convert to 8-bit unsigned integers
+    cv::Mat recon_8u;
+    recon.convertTo(recon_8u, CV_8U);
+    // display image
+    cv::imshow("Reconstructed Image", recon_8u);
+    cv::waitKey(0);
+    std::cout << "reconb rows: " << reconb.rows() << std::endl
+              << "reconb cols: " << reconb.cols() << std::endl;
+
     return 0;
 }
